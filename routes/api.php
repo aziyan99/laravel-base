37c9c8d4e5a66d8db453a880f1c929ed0aaa3097
aziyan99 <rajaazian08@gmail.com>
2021-02-07 01:08:01 +0700

    add routing for role

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\V1\Backend\AuthController;
+use App\Http\Controllers\Api\V1\Backend\RoleController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -26,4 +27,12 @@ Route::group(['prefix' => 'v1'], function () {
 
 Route::group(['prefix' => 'v1', 'middleware' => 'auth:sanctum'], function () {
     Route::post('/logout', [AuthController::class, 'logout']);
+
+    Route::group(['prefix' => 'roles'], function () {
+        Route::get('/', [RoleController::class, 'index']);
+        Route::post('/', [RoleController::class, 'store']);
+        Route::put('/{id}', [RoleController::class, 'update']);
+        Route::delete('/{id}', [RoleController::class, 'destroy']);
+        Route::post('/bulkdelete', [RoleController::class, 'bulkDestroy']);
+    });
 });
