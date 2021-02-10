ad782a8464049304cc14901ae501b04a4f43f470
aziyan99 <rajaazian08@gmail.com>
2021-02-10 17:09:06 +0700

    add access control

--- resources/js/components/backend/partials/Sidemenu.vue
+++ resources/js/components/backend/partials/Sidemenu.vue
@@ -4,7 +4,7 @@
             <b>WEB-NAME</b>
         </div>
         <ul class="c-sidebar-nav">
-            <li class="c-sidebar-nav-item">
+            <li class="c-sidebar-nav-item" v-if="$can('dashbor.lihat')">
                 <router-link class="c-sidebar-nav-link" :to="{name: 'dashboard'}">
                     <i class="c-sidebar-nav-icon c-icon cil-speedometer"></i>
                     Dashbor
@@ -17,25 +17,31 @@
                     Hak Akses & Keamanan
                 </a>
                 <ul class="c-sidebar-nav-dropdown-items">
-                    <li class="c-sidebar-nav-item">
+                    <li class="c-sidebar-nav-item" v-if="$can('role.lihat')">
                         <router-link class="c-sidebar-nav-link" :to="{name: 'role'}">
                             <span class="c-sidebar-nav-icon"></span>
                             Role
                         </router-link>
                     </li>
-                    <li class="c-sidebar-nav-item">
+                    <li class="c-sidebar-nav-item" v-if="$can('permission.lihat')">
                         <router-link class="c-sidebar-nav-link" :to="{name: 'permission'}">
                             <span class="c-sidebar-nav-icon"></span>
                             Permissions
                         </router-link>
                     </li>
-                    <li class="c-sidebar-nav-item">
+                    <li class="c-sidebar-nav-item" v-if="$can('assignpermission.lihat')">
                         <router-link class="c-sidebar-nav-link" :to="{name: 'assignpermission'}">
                             <span class="c-sidebar-nav-icon"></span>
                             Assign Permissions
                         </router-link>
                     </li>
                 </ul>
+            </li>
+            <li class="c-sidebar-nav-item" v-if="$can('pengguna.lihat')">
+                <router-link class="c-sidebar-nav-link" :to="{name: 'user'}">
+                    <i class="c-sidebar-nav-icon c-icon cil-user-plus"></i>
+                    Pengguna
+                </router-link>
             </li>
             <li class="c-sidebar-nav-item">
                 <router-link class="c-sidebar-nav-link" :to="{name: 'profile'}">
